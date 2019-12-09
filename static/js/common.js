cead443c8358bf1240418048fafac5a050661915
leejooy96 <leejooy96@seoul-soft.com>
2019-12-09 17:37:58 +0900

    add base xhr request

--- static/js/common.js
+++ static/js/common.js
@@ -35,3 +35,30 @@ function chr_byte(chr) {
    else
        return 1;
 }
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+let csrfToken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+const addCsrfToken = function(xhr) {
+    xhr.setRequestHeader("X-CSRFToken", csrfToken);
+};
